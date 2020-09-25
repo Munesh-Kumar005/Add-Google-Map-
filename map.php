3fea04c5a27115e7a7c536b8c48e537ac6d0b38c
Munesh <munesh.lalwani1@gmail.com>
2020-09-25 13:49:22 +0500

    My Project

---
+++ map.php
@@ -0,0 +1,11 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Google Map</title>
+</head>
+<body>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14417.20305718498!2d68.3327261!3d25.3947411!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcbdf09e6054835c8!2sAli%20Palace%20Banquet!5e0!3m2!1sen!2s!4v1601023266002!5m2!1sen!2s" width="100%" height="550" frameborder="0" style="border:1;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</body>
+</html>
